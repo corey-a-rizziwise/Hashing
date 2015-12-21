7d2829e9999883df2ffd6c07075ef0f56040f737
corey-a-rizziwise <corey.a.rizziwise@gmail.com>
2015-12-20 23:21:44 -0700

    unzipped the file...

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Samuel Wang on 12/20/15.
+//  Copyright © 2015 Samuel Wang. All rights reserved.
+//
+
+import UIKit
+import Sodium
+
+extension String {
+    func replace(string:String, replacement:String) -> String {
+        return self.stringByReplacingOccurrencesOfString(string, withString: replacement, options: NSStringCompareOptions.LiteralSearch, range: nil)
+    }
+    
+    func removeWhitespace() -> String {
+        return self.replace(" ", replacement: "")
+    }
+    
+    func removeLessThan() -> String {
+        return self.replace("<", replacement: "")
+    }
+    
+    func removeGreaterThan() -> String {
+        return self.replace(">", replacement: "")
+    }
+    
+}
+
+let sodium = Sodium()!
+let message = "b".dataUsingEncoding(NSUTF8StringEncoding)!
+let h = sodium.genericHash.hash(message)! as! NSMutableData
+var randomOne = ((String(sodium.randomBytes.buf(8)! as! NSMutableData).removeWhitespace()).removeLessThan()).removeGreaterThan()
+let randomTwo = ((String(sodium.randomBytes.buf(8)! as! NSMutableData).removeWhitespace()).removeLessThan()).removeGreaterThan()
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var hashedPassword = ((String(h).removeWhitespace()).removeLessThan()).removeGreaterThan()
+        hashedPassword += randomTwo
+        randomOne += hashedPassword
+        let finalHash = randomOne
+        print(finalHash)
+        let index = finalHash.startIndex.advancedBy(16)
+        let endIndex = finalHash.endIndex.advancedBy(-16)
+        let compare = finalHash[Range(start: index, end: endIndex)]
+        print(compare)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
